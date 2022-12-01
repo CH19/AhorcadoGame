71771bbc945bb45689ae325d492c8813885243b6
CH19 <chcc190505@gmail.com>
2022-12-01 16:02:56 -0400

    segundo mensaje hecho en un commit para ver los cambios hecchis, aqui simplemente utilice un commit para modificar los objetos hechos en la clase Descripcciones para que quedara mas estetico

--- src/Pagina1/Descripcciones.java
+++ src/Pagina1/Descripcciones.java
@@ -15,7 +15,7 @@ public class Descripcciones {
 		DescriptWords[2] = "es el lenguaje con el que esta hecho este programa";
 		DescriptWords[3] = "es el tipo de dato principal en la programacion, similar a las constantes";
 		DescriptWords[4] = "es un lenguaje de programacion usado para la mineria de Datos, la IA, el hacking, el Backend, etc";
-		DescriptWords[5] = "es un lenguaje de programacion que sirve para backend y esta";
+		DescriptWords[5] = "es un lenguaje de programacion que sirve para backend y esta Muerto Supuestamente";
 		DescriptWords[6] = "es el simbolo principal de la generacion Z/Milenials";
 		DescriptWords[7] = "es el lenguaje de programacion Scripting mas conocido en el mundo";
 		DescriptWords[8] = "es el protagonista de Naruto";
